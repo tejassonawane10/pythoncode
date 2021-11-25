68b271e216d73013b7a684d010592441b3f444dc
tejassonawane10 <tejassonawane240@gmail.com>
2021-11-25 07:39:37 +0530

    addnumber program

--- addnumber.py
+++ addnumber.py
@@ -1,4 +1,5 @@
 num1=12
 num2=12
-sum= num1+num2
-print('sum of {0} and {1} is {2}'.format(num1,num2,sum))
+num3=12
+sum= num1+num2+num3
+print('sum of {0} and {1} and {2} is {3}'.format(num1,num2,num3,sum))
